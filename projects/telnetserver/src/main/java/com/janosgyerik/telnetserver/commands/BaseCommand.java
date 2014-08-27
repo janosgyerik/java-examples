cefaf3815c7765c9c7cf0c6b5e8598f2118adf8d
Janos Gyerik <janos@kronos>
2014-08-27 22:09:44 +0200

    BaseCommand must be abstract

--- projects/telnetserver/src/main/java/com/janosgyerik/telnetserver/commands/BaseCommand.java
+++ projects/telnetserver/src/main/java/com/janosgyerik/telnetserver/commands/BaseCommand.java
@@ -2,7 +2,7 @@ package com.janosgyerik.telnetserver.commands;
 
 import java.io.File;
 
-public class BaseCommand implements Command {
+public abstract class BaseCommand implements Command {
 	protected final File workdir;
 
 	public BaseCommand(File workdir) {
