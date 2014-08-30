e600ec9cedcb666a4196e973fa0b3219eaa0719e
Janos Gyerik <janos@kronos>
2014-08-30 10:45:11 +0200

    separated TelnetServerRunner from TelnetServer

--- projects/telnetserver/src/main/java/com/janosgyerik/telnetserver/server/TelnetServer.java
+++ projects/telnetserver/src/main/java/com/janosgyerik/telnetserver/server/TelnetServer.java
@@ -6,13 +6,8 @@ import java.net.Socket;
 import java.util.logging.Logger;
 
 public class TelnetServer {
+
 	private static final Logger LOGGER = Logger.getLogger(TelnetServer.class.getSimpleName());
-
-	private static final int PORT = 1234;
-
-	public static void main(String[] args) throws IOException {
-		new TelnetServer().runForever(PORT);
-	}
 
 	public void runForever(int port) throws IOException {
 		ServerSocket serverSocket = new ServerSocket(port);
