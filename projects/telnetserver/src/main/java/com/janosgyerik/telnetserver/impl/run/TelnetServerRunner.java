ba8b2ce71b62f5ccd9ef79c4ae7bc45fbd8beee8
Janos Gyerik <janos@kronos>
2014-08-31 21:58:52 +0200

    multi-threaded server, tmp commit

--- projects/telnetserver/src/main/java/com/janosgyerik/telnetserver/impl/run/TelnetServerRunner.java
+++ projects/telnetserver/src/main/java/com/janosgyerik/telnetserver/impl/run/TelnetServerRunner.java
@@ -1,15 +1,29 @@
 package com.janosgyerik.telnetserver.impl.run;
 
 import com.janosgyerik.telnetserver.impl.server.SimpleTelnetServer;
+import com.janosgyerik.telnetserver.server.TelnetServer;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class TelnetServerRunner {
 
 	private static final int DEFAULT_PORT = 1234;
 
-	public static void main(String[] args) throws IOException {
-		int port = args.length > 0 ? Integer.parseInt(args[0]) : DEFAULT_PORT;
-		new SimpleTelnetServer().runForever(port);
+	public static void main(String[] args) throws IOException, InterruptedException {
+		final int port = args.length > 0 ? Integer.parseInt(args[0]) : DEFAULT_PORT;
+		final TelnetServer server = new SimpleTelnetServer();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					server.start(port);
+				} catch (IOException e) {
+					// e.printStackTrace();
+				}
+			}
+		}).start();
+		TimeUnit.SECONDS.sleep(30);
+		server.shutdown();
 	}
 }
