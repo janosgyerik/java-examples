2c6b1a12ea84beecc737b9f1e6f3d14c4b7d9592
Janos Gyerik <janos@kronos>
2014-11-08 14:50:19 +0100

    reformat + dont be so forgiving

--- src/main/java/com/janosgyerik/examples/lists/ListUtils.java
+++ src/main/java/com/janosgyerik/examples/lists/ListUtils.java
@@ -1,22 +1,21 @@
 package com.janosgyerik.examples.lists;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ListUtils {
-	public static <T> List<List<T>> partition(List<T> orig, int size) {
-		if (size < 1) {
-			throw new IllegalArgumentException("The target partition size must be 1 or greater");
-		}
-		if (orig == null) {
-			return Collections.emptyList();
-		}
-		int origSize = orig.size();
-		List<List<T>> result = new ArrayList<>(origSize / size + 1);
-		for (int i = 0; i < origSize; i += size) {
-			result.add(orig.subList(i, Math.min(i + size, origSize)));
-		}
-		return result;
-	}
+    public static <T> List<List<T>> partition(List<T> orig, int size) {
+        if (orig == null) {
+            throw new NullPointerException("The list to partition must not be null");
+        }
+        if (size < 1) {
+            throw new IllegalArgumentException("The target partition size must be 1 or greater");
+        }
+        int origSize = orig.size();
+        List<List<T>> result = new ArrayList<>(origSize / size + 1);
+        for (int i = 0; i < origSize; i += size) {
+            result.add(orig.subList(i, Math.min(i + size, origSize)));
+        }
+        return result;
+    }
 }
