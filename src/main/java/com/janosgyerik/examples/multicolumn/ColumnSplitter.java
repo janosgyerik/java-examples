52e436ad18cf6a9efd900a2ed95871583330a801
Janos Gyerik <janos@kronos>
2014-11-30 23:00:22 +0100

    added ColumnSplitter interface

---
+++ src/main/java/com/janosgyerik/examples/multicolumn/ColumnSplitter.java
@@ -0,0 +1,33 @@
+package com.janosgyerik.examples.multicolumn;
+
+import java.util.List;
+
+/**
+ * Split a line to columns. Implementation ideas:
+ *
+ * - split by fixed string, for example ","
+ * - split by regex, for example "\s*,\s*", and probably trim() first
+ * - split based on fixed column lengths
+ * - a proper csv parser/splitter could handle embedded newlines
+ */
+public interface ColumnSplitter {
+    /**
+     * Split a "line" of input to String columns.
+     * Note: a proper csv parser could contain embedded newlines.
+     * Note: the returned array always contains at least one entry.
+     *
+     * @param line a non-null input String to split
+     * @return columns found in the line
+     */
+    List<String> splitLine(String line);
+
+    /**
+     * Same as the single argument version, but return at most `limit` columns.
+     * Use `limit = 0` to return all columns.
+     *
+     * @param line a non-null input String to split
+     * @param limit maximum number of columns to return
+     * @return columns found in the line
+     */
+    List<String> splitLine(String line, int limit);
+}
