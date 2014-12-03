1c0cd98e3078e94f6ebd89b7262ad6ce6edd6c6b
Janos Gyerik <janos@kronos>
2014-12-03 10:02:07 +0100

    added DateUtils with tests

---
+++ src/main/java/com/janosgyerik/examples/util/DateUtils.java
@@ -0,0 +1,56 @@
+package com.janosgyerik.examples.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtils {
+    /**
+     * Create a (truncated) date object from year, month, day.
+     * Throws IllegalArgumentException if the parameters don't form
+     * a valid date, for example 2014, 2, 31 or 2014, 99, 99
+     *
+     * @param year Year, for example 2014
+     * @param month Month, for example 2 for February
+     * @param day Day, for example 31st (of December)
+     * @return truncated date (without hours, minutes, seconds and others)
+     */
+    public static Date create(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        validate(year, month, day, calendar);
+        return calendar.getTime();
+    }
+
+    private static void validate(int year, int month, int day, Calendar calendar) {
+        if (calendar.get(Calendar.DAY_OF_MONTH) != day) {
+            throw new IllegalArgumentException(String.format("Invalid day=%s for year=%s and month=%s", day, year, month));
+        }
+        if (calendar.get(Calendar.DAY_OF_MONTH) != day) {
+            throw new IllegalArgumentException(String.format("Invalid day=%s for year=%s and month=%s", day, year, month));
+        }
+    }
+
+    public static String formatYMD(Date date) {
+        return new SimpleDateFormat("yyyy-MM-dd").format(date);
+    }
+
+    public static String getMonthShortName(Date date) {
+        return new SimpleDateFormat("MMM").format(date);
+    }
+
+    public static String getMonthLongName(Date date) {
+        return new SimpleDateFormat("MMMMM").format(date);
+    }
+
+    public static String getDayShortName(Date date) {
+        return new SimpleDateFormat("E").format(date);
+    }
+
+    public static String getDayLongName(Date date) {
+        return new SimpleDateFormat("EEEEE").format(date);
+    }
+}
---
+++ src/test/java/com/janosgyerik/examples/util/DateUtilsTest.java
@@ -0,0 +1,49 @@
+package com.janosgyerik.examples.util;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public class DateUtilsTest {
+    @Test
+    public void test_create_2014_12_03() {
+        Date date = DateUtils.create(2014, 12, 3);
+        assertEquals("2014-12-03", DateUtils.formatYMD(date));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_create_2014_02_31() {
+        DateUtils.create(2014, 2, 31);
+    }
+
+    @Test//(expected = IllegalArgumentException.class)
+    public void test_create_2014_13_01() {
+        DateUtils.create(2014, 13, 1);
+    }
+
+    @Test
+    public void test_getMonthShortName() {
+        Date date = DateUtils.create(2014, 12, 3);
+        assertEquals("Dec", DateUtils.getMonthShortName(date));
+    }
+
+    @Test
+    public void test_getMonthLongName() {
+        Date date = DateUtils.create(2014, 12, 3);
+        assertEquals("December", DateUtils.getMonthLongName(date));
+    }
+
+    @Test
+    public void test_getDayShortName() {
+        Date date = DateUtils.create(2014, 12, 3);
+        assertEquals("Wed", DateUtils.getDayShortName(date));
+    }
+
+    @Test
+    public void test_getDayLongName() {
+        Date date = DateUtils.create(2014, 12, 3);
+        assertEquals("Wednesday", DateUtils.getDayLongName(date));
+    }
+}
