e2e19d0a68a6df6e57830e76ce80c05b6ac032dd
Janos Gyerik <janos@kronos>
2015-02-21 12:00:37 +0100

    ExampleHashMapTest

---
+++ src/main/java/com/janosgyerik/examples/util/ExampleHashMap.java
@@ -0,0 +1,140 @@
+package com.janosgyerik.examples.util;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+public class ExampleHashMap<K, V> implements Map<K, V> {
+
+    private static final int DEFAULT_INITIAL_CAPACITY = 10;
+
+    private final LinkedList<Entry<K, V>>[] storage;
+    private int size = 0;
+
+    private static class Entry<K, V> implements Map.Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+    }
+
+    public ExampleHashMap() {
+        storage = new LinkedList[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        int targetPos = key.hashCode() % storage.length;
+        if (storage[targetPos] != null) {
+            LinkedList<Entry<K, V>> entries = storage[targetPos];
+            for (Entry<K, V> entry : entries) {
+                if (entry.getKey().equals(key)) {
+                    return entry.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        int targetPos = key.hashCode() % storage.length;
+        LinkedList<Entry<K, V>> entries;
+        if (storage[targetPos] == null) {
+            entries = new LinkedList<>();
+            storage[targetPos] = entries;
+        } else {
+            entries = storage[targetPos];
+            for (Entry<K, V> entry : entries) {
+                if (entry.getKey().equals(key)) {
+                    return entry.setValue(value);
+                }
+            }
+        }
+        entries.add(new Entry<>(key, value));
+        ++size;
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        int targetPos = key.hashCode() % storage.length;
+        if (storage[targetPos] != null) {
+            LinkedList<Entry<K, V>> entries = storage[targetPos];
+            for (Entry<K, V> entry : entries) {
+                if (entry.getKey().equals(key)) {
+                    entries.remove(entry);
+                    --size;
+                    return entry.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Map.Entry<K, V>> entrySet() {
+        return null;
+    }
+}
---
+++ src/test/java/com/janosgyerik/examples/util/ExampleHashMapTest.java
@@ -0,0 +1,133 @@
+package com.janosgyerik.examples.util;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class ExampleHashMapTest {
+
+    @Test
+    public void testSize_empty() {
+        Map<Integer, String> map = new ExampleHashMap<>();
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    public void testSize_single() {
+        Map<Integer, String> map = new ExampleHashMap<>();
+        map.put(3, "Jack");
+        assertEquals(1, map.size());
+    }
+
+    @Test
+    public void testSize_after_removal() {
+        Map<Integer, String> map = new ExampleHashMap<>();
+        map.put(3, "Jack");
+        assertEquals(1, map.size());
+        map.remove(3);
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    public void testIsEmpty_init() {
+        Map<Integer, String> map = new ExampleHashMap<>();
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void testIsEmpty_after_put() {
+        Map<Integer, String> map = new ExampleHashMap<>();
+        map.put(3, "Jack");
+        assertFalse(map.isEmpty());
+    }
+
+    @Test
+    public void testIsEmpty_after_remove() {
+        Map<Integer, String> map = new ExampleHashMap<>();
+        map.put(3, "Jack");
+        assertFalse(map.isEmpty());
+        map.remove(3);
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void testPut_degenerate() {
+        Map<Integer, String> map = new ExampleHashMap<>();
+        int size = 100;
+        for (int i = 0; i < size; ++i) {
+            map.put(i, "Jack-" + i);
+        }
+        assertEquals(size, map.size());
+    }
+
+    @Test
+    public void testContainsKey() {
+
+    }
+
+    @Test
+    public void testContainsValue() {
+
+    }
+
+    @Test
+    public void testGet() {
+        Map<Integer, String> map = new ExampleHashMap<>();
+        assertNull(map.get(3));
+        map.put(3, "Jack");
+        assertEquals("Jack", map.get(3));
+        map.remove(3);
+        assertNull(map.get(3));
+    }
+
+    @Test
+    public void testGet_degenerate() {
+        Map<Integer, String> map = new ExampleHashMap<>();
+        int size = 100;
+        for (int i = 0; i < size; ++i) {
+            map.put(i, "Jack-" + i);
+        }
+        assertEquals(size, map.size());
+
+        for (int i = 0; i < size; ++i) {
+            assertEquals("Jack-" + i, map.get(i));
+        }
+    }
+
+    @Test
+    public void testPut() {
+
+    }
+
+    @Test
+    public void testRemove() {
+
+    }
+
+    @Test
+    public void testPutAll() {
+
+    }
+
+    @Test
+    public void testClear() {
+
+    }
+
+    @Test
+    public void testKeySet() {
+
+    }
+
+    @Test
+    public void testValues() {
+
+    }
+
+    @Test
+    public void testEntrySet() {
+
+    }
+}
