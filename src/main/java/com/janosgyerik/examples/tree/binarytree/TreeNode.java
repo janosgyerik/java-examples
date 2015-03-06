3d2cbe179a4dd7ca2c3bc2d21905abf9a2cf59dc
Janos Gyerik <janos@kronos>
2015-03-06 11:25:42 +0100

    added preorderiterator

---
+++ src/main/java/com/janosgyerik/examples/tree/binarytree/TreeNode.java
@@ -0,0 +1,11 @@
+package com.janosgyerik.examples.tree.binarytree;
+
+public class TreeNode<T> {
+    public T val;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
+
+    public TreeNode(T x) {
+        val = x;
+    }
+}
---
+++ src/test/java/com/janosgyerik/examples/tree/binarytree/TraversalTest.java
@@ -0,0 +1,42 @@
+package com.janosgyerik.examples.tree.binarytree;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TraversalTest {
+
+    // example tree from http://en.wikipedia.org/wiki/Tree_traversal
+    private final TreeNode<Character> root;
+
+    public TraversalTest() {
+        root = new TreeNode<>('F');
+        root.left = new TreeNode<>('B');
+        root.left.left = new TreeNode<>('A');
+        root.left.right = new TreeNode<>('D');
+        root.left.right.left = new TreeNode<>('C');
+        root.left.right.right = new TreeNode<>('E');
+        root.right = new TreeNode<>('G');
+        root.right.right = new TreeNode<>('I');
+        root.right.right.left = new TreeNode<>('H');
+    }
+
+    static <T> List<T> iterateToList(Iterator<T> iterator) {
+        List<T> list = new LinkedList<>();
+        while (iterator.hasNext()) {
+            list.add(iterator.next());
+        }
+        return list;
+    }
+
+    @Test
+    public void testPreOrderIterator() {
+        PreOrderIterator<Character> iterator = new PreOrderIterator<>(root);
+        assertEquals(Arrays.asList('F', 'B', 'A', 'D', 'C', 'E', 'G', 'I', 'H'), iterateToList(iterator));
+    }
+}
