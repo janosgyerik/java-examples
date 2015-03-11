4b93a522dae197c915af2e00c68c1a649f15dc3a
Janos Gyerik <janos@kronos>
2015-03-11 23:18:49 +0100

    added TreeUtils.size

--- src/main/java/com/janosgyerik/examples/tree/binarytree/TreeUtils.java
+++ src/main/java/com/janosgyerik/examples/tree/binarytree/TreeUtils.java
@@ -67,6 +67,13 @@ public class TreeUtils {
         return 1 + Math.max(getHeight(root.left), getHeight(root.right));
     }
 
+    public static int size(TreeNode<?> root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + size(root.left) + size(root.right);
+    }
+
     public static <T extends Comparable<T>> boolean isBinarySearchTree(TreeNode<T> root, T minValue, T maxValue) {
         return root == null ||
                 minValue.compareTo(root.val) < 0 && root.val.compareTo(maxValue) < 0
--- src/test/java/com/janosgyerik/examples/tree/binarytree/TreeUtilsTest.java
+++ src/test/java/com/janosgyerik/examples/tree/binarytree/TreeUtilsTest.java
@@ -35,6 +35,44 @@ public class TreeUtilsTest {
     }
 
     @Test
+    public void test_size_empty() {
+        assertEquals(0, TreeUtils.size(null));
+    }
+
+    @Test
+    public void test_size_singleton() {
+        assertEquals(1, TreeUtils.size(new TreeNode<>(3)));
+    }
+
+    @Test
+    public void test_size_leftHigher() {
+        TreeNode<Integer> root = new TreeNode<>(1);
+        root.left = new TreeNode<>(2);
+        assertEquals(2, TreeUtils.size(root));
+    }
+
+    @Test
+    public void test_size_rightHigher() {
+        TreeNode<Integer> root = new TreeNode<>(1);
+        root.left = new TreeNode<>(2);
+        root.right = new TreeNode<>(3);
+        root.right.right = new TreeNode<>(4);
+        assertEquals(4, TreeUtils.size(root));
+    }
+
+    @Test
+    public void test_size_complete() {
+        TreeNode<Integer> root = new TreeNode<>(1);
+        root.left = new TreeNode<>(2);
+        root.left.left = new TreeNode<>(3);
+        root.left.right = new TreeNode<>(4);
+        root.right = new TreeNode<>(5);
+        root.right.left = new TreeNode<>(6);
+        root.right.right = new TreeNode<>(7);
+        assertEquals(7, TreeUtils.size(root));
+    }
+
+    @Test
     public void test_isBinarySearchTree_empty() {
         assertTrue(TreeUtils.isBinarySearchTree(null, 1, 1));
     }
