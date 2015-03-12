7be8aacba31c30583a4095d17bcdde118e049f0a
Janos Gyerik <janos@kronos>
2015-03-12 16:33:23 +0100

    added NumberUtils.factorize, TODO

---
+++ src/main/java/com/janosgyerik/examples/util/NumberUtils.java
@@ -0,0 +1,14 @@
+package com.janosgyerik.examples.util;
+
+import java.util.SortedSet;
+
+public class NumberUtils {
+
+    private NumberUtils() {
+        // utility class, forbidden constructor
+    }
+
+    public static SortedSet<Integer> factorize(int n) {
+        throw new UnsupportedOperationException("TODO, by making a sieve first with lowest prime divisor");
+    }
+}
