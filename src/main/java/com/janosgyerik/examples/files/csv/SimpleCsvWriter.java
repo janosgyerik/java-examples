602f6cf48166ddc7d0559278886297f85dd3d8ab
Janos Gyerik <janos@kronos>
2015-07-05 22:59:17 +0200

    added SimpleCsvWriter

---
+++ src/main/java/com/janosgyerik/examples/files/csv/SimpleCsvWriter.java
@@ -0,0 +1,42 @@
+package com.janosgyerik.examples.files.csv;
+
+import java.io.*;
+import java.util.Collection;
+
+public class SimpleCsvWriter {
+
+    private static final String DEFAULT_NEWLINE = System.getProperty("line.separator");
+    private static final String DEFAULT_SEPARATOR = ",";
+
+    private final PrintWriter writer;
+    private final String separator;
+    private final String newline;
+
+    public SimpleCsvWriter(File file, String separator) throws FileNotFoundException {
+        writer = new PrintWriter(file);
+        this.separator = separator;
+        this.newline = DEFAULT_NEWLINE;
+    }
+
+    public SimpleCsvWriter(File file) throws FileNotFoundException {
+        this(file, DEFAULT_SEPARATOR);
+    }
+
+    public SimpleCsvWriter(String path) throws FileNotFoundException {
+        this(new File(path));
+    }
+
+    public void writeRow(Object... columns) {
+        writer.write(columns[0].toString());
+        for (int i = 1; i < columns.length; ++i) {
+            writer.write(separator);
+            writer.write(columns[i].toString());
+        }
+        writer.write(newline);
+        writer.flush();
+    }
+
+    public void close() {
+        writer.close();
+    }
+}
