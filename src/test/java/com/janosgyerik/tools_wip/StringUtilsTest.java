d6ae95134e6d6f9f4d7dae63baee3f799d61cc25
Janos Gyerik <janos@kronos>
2016-01-03 18:46:31 +0100

    improved test case names for StringUtils.replace

--- src/test/java/com/janosgyerik/tools_wip/StringUtilsTest.java
+++ src/test/java/com/janosgyerik/tools_wip/StringUtilsTest.java
@@ -7,17 +7,17 @@ import static org.junit.Assert.assertEquals;
 
 public class StringUtilsTest {
     @Test
-    public void testSingleExact() {
+    public void test_replace_one_pattern_once() {
         assertEquals("bar", replace("foo", new String[]{"foo"}, new String[]{"bar"}));
     }
 
     @Test
-    public void testReplaceTwice() {
+    public void test_replace_one_pattern_twice() {
         assertEquals("barbar", replace("foofoo", new String[]{"foo"}, new String[]{"bar"}));
     }
 
     @Test
-    public void testReplaceTwoPatterns() {
+    public void test_replace_two_patterns_simultaneously() {
         assertEquals("barbaz", replace("foobar",
                 new String[]{"foo", "bar"},
                 new String[]{"bar", "baz"}));
@@ -25,19 +25,19 @@ public class StringUtilsTest {
 
     //TODO invalid input, should throw exception
     //@Test
-    public void testDuplicatePatterns() {
+    public void test_ambiguous_patterns_should_throw() {
         assertEquals("barbar", replace("foobar",
                 new String[]{"foo", "foo"},
                 new String[]{"bar", "baz"}));
     }
 
     @Test
-    public void testReplaceNone() {
+    public void test_no_matches() {
         assertEquals("foofoo", replace("foofoo", new String[]{"x"}, new String[]{"bar"}));
     }
 
     @Test
-    public void testStory() {
+    public void test_replace_multiple_simultaneous_patterns() {
         assertEquals("Once upon a foo, there was a bar and a baz, and another bar and a cat.",
                 replace("Once upon a baz, there was a foo and a bar, and another foo and a cat.",
                         new String[]{"foo", "bar", "baz"},
