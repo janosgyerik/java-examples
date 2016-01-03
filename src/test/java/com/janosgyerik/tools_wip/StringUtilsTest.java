7a74a0cfb56dce3bb821416ec70cc86d3156486e
Janos Gyerik <janos@kronos>
2016-01-03 18:41:00 +0100

    simplify test using static import

--- src/test/java/com/janosgyerik/tools_wip/StringUtilsTest.java
+++ src/test/java/com/janosgyerik/tools_wip/StringUtilsTest.java
@@ -2,22 +2,23 @@ package com.janosgyerik.tools_wip;
 
 import org.junit.Test;
 
+import static com.janosgyerik.tools_wip.StringUtils.replace;
 import static org.junit.Assert.assertEquals;
 
 public class StringUtilsTest {
     @Test
     public void testSingleExact() {
-        assertEquals("bar", StringUtils.replace("foo", new String[]{"foo"}, new String[]{"bar"}));
+        assertEquals("bar", replace("foo", new String[]{"foo"}, new String[]{"bar"}));
     }
 
     @Test
     public void testReplaceTwice() {
-        assertEquals("barbar", StringUtils.replace("foofoo", new String[]{"foo"}, new String[]{"bar"}));
+        assertEquals("barbar", replace("foofoo", new String[]{"foo"}, new String[]{"bar"}));
     }
 
     @Test
     public void testReplaceTwoPatterns() {
-        assertEquals("barbaz", StringUtils.replace("foobar",
+        assertEquals("barbaz", replace("foobar",
                 new String[]{"foo", "bar"},
                 new String[]{"bar", "baz"}));
     }
@@ -25,20 +26,20 @@ public class StringUtilsTest {
     //TODO invalid input, should throw exception
     //@Test
     public void testDuplicatePatterns() {
-        assertEquals("barbar", StringUtils.replace("foobar",
+        assertEquals("barbar", replace("foobar",
                 new String[]{"foo", "foo"},
                 new String[]{"bar", "baz"}));
     }
 
     @Test
     public void testReplaceNone() {
-        assertEquals("foofoo", StringUtils.replace("foofoo", new String[]{"x"}, new String[]{"bar"}));
+        assertEquals("foofoo", replace("foofoo", new String[]{"x"}, new String[]{"bar"}));
     }
 
     @Test
     public void testStory() {
         assertEquals("Once upon a foo, there was a bar and a baz, and another bar and a cat.",
-                StringUtils.replace("Once upon a baz, there was a foo and a bar, and another foo and a cat.",
+                replace("Once upon a baz, there was a foo and a bar, and another foo and a cat.",
                         new String[]{"foo", "bar", "baz"},
                         new String[]{"bar", "baz", "foo"})
         );
