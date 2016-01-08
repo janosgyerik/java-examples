a2bbc5bde699167736452bc5fb083f3677b2579c
Janos Gyerik <janos@kronos>
2016-01-09 00:02:52 +0100

    moved tree utils to java-tools

--- src/test/java/com/janosgyerik/examples/tree/binarytree/BinarySearchTreeImplTest.java
+++
@@ -1,8 +0,0 @@
-package com.janosgyerik.examples.tree.binarytree;
-
-public class BinarySearchTreeImplTest extends BinarySearchTreeTest {
-    @Override
-    protected <T extends Comparable<T>> BinarySearchTree<T> newBinarySearchTree() {
-        return new BinarySearchTreeImpl<>();
-    }
-}
